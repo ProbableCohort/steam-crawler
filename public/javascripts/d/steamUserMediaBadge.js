0183ec0477bae4fe0153f1bd517f02898a04d356
Brent Haertlein <brenthaertlein@gmail.com>
2016-10-12 22:08:24 -0400

    steamusermediabadge directive

---
+++ public/javascripts/d/steamUserMediaBadge.js
@@ -0,0 +1,71 @@
+/* global angular */
+(function(angular) {
+  angular.module('crawlerDirectives').directive('steamUserMediaBadge', steamUserMediaBadge);
+
+  steamUserMediaBadge.$inject = [];
+
+  function steamUserMediaBadge() {
+      var directive = {
+        restrict: 'E',
+        replace: true,
+        require: 'ngModel',
+        templateUrl: 'javascripts/d/templates/steamUserMediaBadge.html',
+        scope: {
+          steamUser : '=ngModel'
+        },
+        controller: ctrlFn
+      }
+
+      return directive;
+
+      /////////////////
+
+      function ctrlFn($scope) {
+
+        console.log($scope);
+
+        $scope.determinePersonaState = function(entity) {
+          console.log(entity);
+          if (!entity)
+            return;
+          var state;
+          var stateId = entity.personastate;
+          switch(stateId) {
+            case 0:
+              state = "Offline";
+              entity.status = 2;
+              break;
+            case 1:
+              state = "Online";
+              entity.status = 0;
+              break;
+            case 2:
+              state = "Busy";
+              entity.status = 2;
+              break;
+            case 3:
+              state = "Away";
+              entity.status = 1;
+              break;
+            case 4:
+              state = "Snooze";
+              entity.status = 2;
+              break;
+            case 5:
+              state = "Looking To Trade";
+              entity.status = 1;
+              break;
+            case 6:
+              state = "Looking To Play";
+              entity.status = 1;
+              break;
+            default:
+              break;
+          }
+          return state;
+        }
+
+      }
+  }
+
+})(angular);
