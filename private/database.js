d0792137206cc221bb5b5e1c7f7aecf6721647b2
Brent Haertlein <brenthaertlein@gmail.com>
2016-10-13 00:00:55 -0400

    db_uri

--- private/database.js
+++ private/database.js
@@ -1,6 +1,6 @@
 var KEYS = require('./keys')
 
-var url = 'mongodb://' + KEYS.DB_USER + ':' + KEYS.DB_PASSWORD + '@ds049651.mlab.com:49651/steam-crawler';
+var url = 'mongodb://' + KEYS.DB_USER + ':' + KEYS.DB_PASSWORD + '@' + KEYS.DB_URI;
 
 module.exports = {
   'url' : url
