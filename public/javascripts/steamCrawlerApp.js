3e2c78d77c9ea923993d2acaaf24173ab28e7243
Brent Haertlein <brenthaertlein@gmail.com>
2016-10-16 21:21:33 -0400

    vertililze

--- public/javascripts/steamCrawlerApp.js
+++ public/javascripts/steamCrawlerApp.js
@@ -7,10 +7,9 @@
   });
 
   angular.module('steamCrawlerApp').config(['$httpProvider', function($httpProvider) {
-      $httpProvider.defaults.useXDomain = true;
-      delete $httpProvider.defaults.headers.common['X-Requested-With'];
-    }
-  ]);
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
+  }]);
 
   angular.module('crawlerServices', []);
   angular.module('crawlerControllers', []);
